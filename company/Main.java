b78c11508192416639c7e9957273937a779be51e
Yahya Abdul-Hakeem <42775710+ya7ya1994@users.noreply.github.com>
2020-03-06 17:35:12 -0800

    Added Source code of Symmetric-Tree

---
+++ company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+public void printTree(ArrayList<Integer> tree)
+{
+    for(Integer node:tree)
+    {
+        System.out.println(node);
+    }
+}
+    public static void main(String[] args) {
+
+    }
+}
---
+++ company/TreeNode.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
